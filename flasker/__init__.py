5130582e3abfe330f46892e3fe681a2e8006b321
Alexandre <almmello@gmail.com>
2022-06-28 20:33:15 -0300

    exercise OK

--- flasker/__init__.py
+++ flasker/__init__.py
@@ -1,40 +1,24 @@
-# Import your dependencies
-from flask import Flask, jsonify
-import os
+from flask import Flask, jsonify, request
+from models import setup_db, Plant
 
-# Define the create_app function
 def create_app(test_config=None):
-  # Create and configure the app
-  # Include the first parameter: Here, __name__is the name of the current Python module.
+    app = Flask(__name__)
+    setup_db(app)
 
-  app = Flask(__name__, instance_relative_config=True)
-  app.config.from_mapping(
-    SECRET_KEY='dev', 
-    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-  )
+    @app.route('/plants', methods=['GET','POST'])
+    #@cross_origin
+    def get_plants():
+        # Implement pagination
+        page = request.args.get('page', 1, type=int)
+        start = (page - 1) * 10
+        end = start + 10
 
-  if test_config is None:
-  # load the instance config, if it exists, when not testing
-    app.config.from_pyfile('config.py', silent=True)
-  else:
-  # load the test config if passed in
-    app.config.from_mapping(test_config)
+        plants = Plant.query.all()
+        formatted_plants = [plant.format() for plant in plants]
+        return jsonify({
+            'success': True,
+            'plants':formatted_plants[start:end],
+            'total_plants':len(formatted_plants)
+            })
 
-  try:
-    os.makedirs(app.instance_path)
-  except OSError:
-    pass
-
-
-  @app.route('/')
-  def hello_world():
-    return jsonify({'message':'Hello, World!'})
-
-
-  @app.route('/smiley')
-  def smiley():
-    return ':)'
-
-  
-  return app
-
+    return app
